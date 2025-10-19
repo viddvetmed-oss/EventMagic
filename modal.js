713f0a028ccf01b562f38aa095c908922dbf2d67
viddvetmed-oss <vidd.vet.med@gmail.com>
2025-10-19 10:43:22 +0300

    Delete modal.js

--- modal.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault(); // щоб не оновлювала сторінку
-  document.getElementById("successModal").style.display = "flex";
-  this.reset(); // очистити форму після відправки
-});
-
-document.getElementById("closeModal").addEventListener("click", function() {
-  document.getElementById("successModal").style.display = "none";
-});
